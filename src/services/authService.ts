eb7fe76f782a7146f8071e0839a6ad8975efc783
QUADRRATOM <114084077+quadratom@users.noreply.github.com>
2025-03-02 13:46:37 -0600

    Update authService.ts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = 'http://147.93.40.101:3332'
 
 export const authService = {
   signup: (data: { email: string; password: string }) => axios.post(`${API_URL}/auth/signup`, data),
