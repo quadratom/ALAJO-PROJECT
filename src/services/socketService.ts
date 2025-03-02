fc62a4f22a7882c13d9e3e0faec0cc5716c9f9c3
QUADRRATOM <114084077+quadratom@users.noreply.github.com>
2025-03-02 13:47:18 -0600

    Update socketService.ts

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,5 +1,5 @@
 // src/services/socketService.ts
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = 'http://localhost:3000' // Adjust if needed
+const SOCKET_URL = 'http://147.93.40.101:3332' // Adjust if needed
 export const socket = io(SOCKET_URL, { transports: ['websocket'] })
